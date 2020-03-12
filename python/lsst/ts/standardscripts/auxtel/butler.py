5ec4dee618e1833ad2e3ec5d317dafd7715e46ff
Robert Lupton the Good <rhl@astro.princeton.edu>
2020-03-11 22:56:10 -0400

    Fake the fake images better, to allow display.mtv to work

--- python/lsst/ts/standardscripts/auxtel/butler.py
+++ python/lsst/ts/standardscripts/auxtel/butler.py
@@ -1,6 +1,7 @@
 """Fake up the butler too, always returning a rather boring image"""
 
 import numpy as np
+import lsst.daf.base as dafBase
 import lsst.afw.image as afwImage
 
 class Butler():
@@ -12,7 +13,14 @@ class Butler():
         return True
     
     def get(self, dataType, dataId, **kwargs):
-        exp = afwImage.ExposureF(4036, 4000)
+        nx, ny = 4036, 4000
+        exp = afwImage.ExposureF(nx, ny)
+        exp.image.array = np.random.normal(size=nx*ny).astype(np.float32).reshape(ny, nx)
+
+        md = dafBase.PropertyList()
+        md.set("OBJECT", "unknown")
+        exp.setMetadata(md)
+        
         cen = 2500, 2300
         exp.image.array[cen[0] - 10: cen[0] + 10, cen[1] - 10: cen[1] + 15] = 2000
         exp.image.array[cen[0], cen[1]] = 3000
